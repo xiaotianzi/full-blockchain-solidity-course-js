8ce05033af730a76f8c299ef6e98450f5c65d9d2
xiaotianzi <aisuo@live.cn>
2023-07-06 00:56:12 +0800

    practice0705

--- 09-hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
+++ 09-hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -1,4 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract Raffle {}
+error Raffle__NotEnoughETHEntered();
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract Raffle is VRFConsumerBaseV2 {
+    /* State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(
+        address vrfCoodinatorV2,
+        uint256 entranceFee
+    ) VRFConsumerBaseV2(vrfCoodinatorV2) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    /** View / Pure functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
