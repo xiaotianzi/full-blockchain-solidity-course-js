8c318531b4dcfb84f70d70c37bf9a060f3ff17a4
xiaotianzi <aisuo@live.cn>
2023-07-10 09:13:22 +0800

    update deploy

--- 09-hardhat-smartcontract-lottery-fcc/deploy/00-deploy-mocks.js
+++ 09-hardhat-smartcontract-lottery-fcc/deploy/00-deploy-mocks.js
@@ -1,12 +1,25 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
 const { developmentChains } = require("../helper-hardhat-config");
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is the premium. It costs 0.25 LINK per request.
+const GAS_PRICE_LINK = 1e9 // link per gas. caculated value based on the gas price of the chain.
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
-    const chainId = network.config.chainId;
+    const args = [BASE_FEE, GAS_PRICE_LINK]
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying mocks...");
+        //deploy a mock vrfcoordinator...
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args
+        })
+        log("Mocks Deployed!")
+        log("---------------------------------------------------")
     }
 };
+
+module.exports.tags = ["all", "mocks"]
