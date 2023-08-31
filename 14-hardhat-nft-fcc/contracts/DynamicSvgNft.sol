5b8c958698f4f2f7426bcc4b5618b30f80a0f9db
xiaotianzi <aisuo@live.cn>
2023-08-31 17:42:34 +0800

    update lesson 14

---
+++ 14-hardhat-nft-fcc/contracts/DynamicSvgNft.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
+
+contract DynamicSvgNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string private s_lowImageURI;
+    string private s_highImageURI;
+    string private constant base64EncodedSvgPrefix =
+        "data:image/svg+xml;base64,";
+
+    constructor(
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("Dynamic SVG NFT", "DSN") {
+        s_tokenCounter = 0;
+    }
+
+    function svgToImageURI(
+        string memory svg
+    ) public pure returns (string memory) {
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return
+            string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter += 1;
+    }
+}
